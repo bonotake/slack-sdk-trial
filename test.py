92259aa2d3309f31c5b19e1d89b10615998e3d7d
Takeo Imai <takeo.bono@gmail.com>
2021-06-24 15:43:41 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return name
+
+@app.route('/good', methods=['POST'])
+def good():
+    data = request.get_data()
+    print(data)
+    return data
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
